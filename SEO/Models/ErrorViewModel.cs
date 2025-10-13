a548c8b8e7c6a40336267a72dad6317d26a5e7f1
Mostafa from Work <mostafa.bjn.80@gmail.com>
2025-10-13 10:17:50 +0330

    Add project files.

---
+++ SEO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
